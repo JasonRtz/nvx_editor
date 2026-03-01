77e4b44f09c7f972868b33968e68efbc40a1263a
JasonRtz <jason.rexhaj11@gmail.com>
2026-03-01 21:58:35 +0200

    Adding file structure, and initial code for the text area.

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt6.QtWidgets import (QMainWindow, QTextEdit, QFileDialog, QMessageBox)
+from PyQt6.QtGui import QAction, QIcon
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("NVX Editor")
+        self.resize(800, 600)
+
+        self.editor = QTextEdit()
+        self.setCentralWidget(self.editor)
+
+        self.view()
+
+    def view(self):
+        menu_bar = self.menuBar()
---
+++ src/main.py
@@ -0,0 +1,13 @@
+from app import App
+from PyQt6.QtWidgets import QApplication
+import sys
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
